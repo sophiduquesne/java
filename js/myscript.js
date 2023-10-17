4f0dcd8ec1c3d498a00150caeeb88104734c2115
sophiduquesne <142498460+sophiduquesne@users.noreply.github.com>
2023-10-17 11:28:57 +0000

    ok

---
+++ js/myscript.js
@@ -0,0 +1,9 @@
+function trocarFrase(){
+    var fraseAtual=document.getElementById("frase");
+    fraseAtual.innerHTML= "Qualquer frase!";
+    fraseAtual.style.color= "purple";
+    fraseAtual.style.fontSize= "25px";
+    fraseAtual.style.backgroundColor= "lightblue";
+    document.body.style.backgroundColor= "lightgreen";
+    fraseAtual.innerHTML= "<img src=\'img/rapunzel.jpg\' />"
+}
